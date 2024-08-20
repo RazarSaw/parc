e414cbf4c0b7ea552d4ff669bf6d29fdf925b982
Razar Saw <razar.saw@gmail.com>
2024-08-20 00:25:22 -0600

    Rerouted URLS to work with /parc/ subdirectory, and changed <Image> to <img>.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,13 +1,11 @@
 import Link from "next/link";
-import Image from "next/image";
-import banner from "./../../public/images/logos/PARC_banner.png"
 
 export default function Header() {
   return (
     <>
       <header className="grid place-items-center gap-8 px-8 py-16 bg-[url('/parc/images/headers/home.png')] bg-cover bg-bottom min-h-[40vh] bg-blend-multiply bg-gray-500 text-center">
         <Link href={"/"} className="hover:scale-[1.05] transition-all">
-          <Image src={banner} alt="Parkland Amateur Radio Club banner"/>
+          <img src="/parc/images/logos/PARC-banner.png" alt="Parkland Amateur Radio Club banner"/>
         </Link>
         <p className="text-3xl text-white font-semibold">
           Interested In Our Network?
@@ -20,7 +18,7 @@ export default function Header() {
             <p>Visit SASKWARN (Wireless Amateur Radio Network)</p>
           </a>
           <a
-            href="parc/community-and-support/membership-directors-and-sponsors"
+            href="/parc/community-and-support/membership-directors-and-sponsors"
             className="bg-blue-400 hover:bg-blue-500 rounded-[8px] p-4 text-white font-semibold hover:scale-[1.05] transition-all"
           >
             <p>Membership</p>
