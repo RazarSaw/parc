bf11292023cf92fa080c386016be27562ac1e885
Razar Saw <razar.saw@gmail.com>
2024-08-20 00:44:53 -0600

    Implemented new link component, added new description for 'Why Would Anyone Join The Hobby?', and edited apostrophes and quote tags to work with HTML escape codes

--- app/ham-radio-resources/ham-radio-info/page.jsx
+++ app/ham-radio-resources/ham-radio-info/page.jsx
@@ -1,60 +1,39 @@
-import Header from "./header"
+import Header from "./header";
+import Links from "./../../components/Links";
+
 export default function HamRadioInfo() {
+  const links = [
+    {
+      title: "Getting Started",
+      href: "https://ised-isde.canada.ca/site/spectrum-management-telecommunications/en/spectrum-allocation/amateur-radio-service",
+    },
+    {
+      title: "Examiner Search",
+      href: "https://apc-cap.ic.gc.ca/pls/apc_anon/query_examiner_amat$.startup",
+    },
+    {
+      title: "Shortwave",
+      href: "https://www.radioddity.com/blogs/all/shortwave-radio",
+    },
+    {
+      title: "Q Codes",
+      href: "./qcode.pdf",
+    },
+    {
+      title: "RAC",
+      href: "https://www.rac.ca/",
+    },
+    {
+      title: "ISED",
+      href: "https://ised-isde.canada.ca/site/amateur-radio-operator-certificate-services/en",
+    },
+  ];
   return (
     <>
-    <Header />
+      <Header />
       <main className="bg-neutral-800">
         <div className="container mx-auto py-16 p-8 flex flex-col gap-16">
-          <section className="flex flex-row gap-8 flex-wrap">
-            <a
-              href="https://ised-isde.canada.ca/site/spectrum-management-telecommunications/en/spectrum-allocation/amateur-radio-service"
-              className=" flex-1"
-            >
-              <article className="p-4 aspect-[16/3] bg-neutral-700 grid place-items-center rounded-[8px] transition-all hover:scale-[1.05]">
-                <p>Getting Started</p>
-              </article>
-            </a>
-            <a
-              href="https://apc-cap.ic.gc.ca/pls/apc_anon/query_examiner_amat$.startup"
-              className=" flex-1"
-            >
-              <article className="p-4 aspect-[16/3] bg-neutral-700 grid place-items-center rounded-[8px] transition-all hover:scale-[1.05]">
-                <p>Examiner Search</p>
-              </article>
-            </a>
-            <a
-              href="https://www.radioddity.com/blogs/all/shortwave-radio"
-              className=" flex-1"
-            >
-              <article className="p-4 aspect-[16/3] bg-neutral-700 grid place-items-center rounded-[8px] transition-all hover:scale-[1.05]">
-                <p>Shortwave</p>
-              </article>
-            </a>
-            <a href="./qcode.pdf" className=" flex-1">
-              <article className="p-4 aspect-[16/3] bg-neutral-700 grid place-items-center rounded-[8px] transition-all hover:scale-[1.05]">
-                <p>Q Codes</p>
-              </article>
-            </a>
-            <a href="https://www.rac.ca/" className=" flex-1">
-              <article
-                className="p-4 aspect-[16/3] bg-neutral-700 grid place-items-center rounded-[8px] transition-all hover:scale-[1.05]"
-                title="Radio Amateurs of Canada"
-              >
-                <p>RAC</p>
-              </article>
-            </a>
-            <a
-              href="https://ised-isde.canada.ca/site/amateur-radio-operator-certificate-services/en"
-              className=" flex-1"
-            >
-              <article
-                className="p-4 aspect-[16/3] bg-neutral-700 grid place-items-center rounded-[8px] transition-all hover:scale-[1.05]"
-                title="Innovation, Science, and Economic Development Canada (Industry Canada)"
-              >
-                <p>ISED</p>
-              </article>
-            </a>
-          </section>
+          <Links props={links} />
           <section className="flex flex-col gap-8 text-white">
             <h3>What Is Amateur Radio?</h3>
             <p>
@@ -81,73 +60,52 @@ export default function HamRadioInfo() {
               own antennas, partake in circuitry, and explore physics. <br />
               <br />
               The great thing is: <strong>anybody can participate.</strong> As
-              long as you&apos;re a certified amateur operator, or in the presence of
-              one, you&apos;ll be able to get on the air in no time.
+              long as you&apos;re a certified amateur operator, or in the
+              presence of one, you&apos;ll be able to get on the air in no time.
             </p>
           </section>
         </div>
       </main>
 
       <section className="bg-[url('/parc/Yagi%20Uda.jpg')] bg-cover bg-no-repeat min-h-[40vh] bg-black/50 bg-blend-multiply grid place-items-center text-white font-bold text-2xl">
-        <p className="container mx-auto p-8 text-center">&quot;Insert some weird quote RJM!&quot;</p>
+        <p className="container mx-auto p-8 text-center">
+          &quot;Insert some weird quote RJM!&quot;
+        </p>
       </section>
       <main className="bg-neutral-800 text-white">
-        <div className="container mx-auto py-16 p-8 flex flex-col gap-16">
+        <div className="container mx-auto py-16 p-8 flex flex-col gap-8">
           <h3>Why Would Anyone Join The Hobby?</h3>
           <p>
             <img
               src="/parc/Newspaper.png"
-              className="float-left lg:w-1/3 w-full rounded-[8px] mb-8 mr-8"
+              className="float-left lg:w-1/3 w-full rounded-[8px] mb-4 mr-8"
             />
-            Amateur radio is a lot more than just talk to each other with radio
-            waves. The hobby involves STEM, physics, and social skills! Lorem
-            ipsum dolor sit amet consectetur adipisicing elit. Deserunt corporis
-            necessitatibus, a error illum dolorem asperiores est consectetur
-            numquam architecto ducimus voluptatibus nesciunt id, quia sunt
-            pariatur. Laborum provident minima magnam modi delectus tempora
-            fugit placeat eaque dolore cum possimus, quas necessitatibus
-            corrupti. Quam similique illo beatae delectus eligendi,
-            necessitatibus harum quae non, laudantium tempora repudiandae
-            laborum? Recusandae perferendis aliquam vel molestiae, nesciunt
-            atque omnis dolor itaque possimus? Nam provident quam totam enim
-            modi culpa voluptatibus sunt nobis facilis, consequuntur magnam
-            doloribus, voluptate aut neque velit iste deserunt quidem optio
-            suscipit illo! Odio maiores ipsa voluptates minus. Suscipit enim
-            temporibus eum mollitia voluptate ipsum dignissimos. Dignissimos
-            pariatur dolorum nemo culpa? At, nisi ipsam placeat maiores rerum
-            maxime perferendis culpa quas vel praesentium doloribus esse quis,
-            voluptates in harum voluptatum, quisquam nihil corrupti commodi
-            consequatur excepturi pariatur similique omnis saepe? At atque iusto
-            tenetur ullam facere laboriosam quae perspiciatis itaque officiis
-            harum adipisci quas eum distinctio rem molestias quia, eius dolor
-            placeat omnis nisi nihil voluptas odio sunt. Dolorum, quo. Vero,
-            neque deleniti commodi nihil dignissimos dolorum voluptatem maxime,
-            placeat doloremque in sunt soluta vitae modi distinctio ex, deserunt
-            numquam quae tempora beatae! Ipsum repellendus commodi quisquam cum
-            similique cumque vero laboriosam enim velit eaque praesentium
-            fugiat, voluptatibus iusto labore sint ea laborum hic suscipit fuga
-            eveniet culpa odio quia non nisi? Provident ab perferendis inventore
-            quas, alias veritatis dicta corrupti earum, quod, debitis quibusdam!
-            Iure explicabo repellat maiores dicta tempore laboriosam qui placeat
-            ut ea hic facere tenetur provident inventore aliquid dignissimos
-            ipsum reiciendis eligendi architecto, consectetur porro laborum
-            perferendis velit recusandae asperiores! Voluptas animi, amet quidem
-            nostrum aspernatur eaque error iusto ratione doloribus, commodi
-            ducimus fugit voluptate aperiam autem laborum id molestiae assumenda
-            dolore consequuntur veritatis. Maxime corrupti accusantium, suscipit
-            deserunt illo fuga corporis praesentium similique nostrum dolorem
-            fugiat minus dicta aliquid excepturi blanditiis officiis a hic,
-            libero eum voluptate alias ad facere autem voluptas? Pariatur vitae
-            nisi accusantium, modi voluptatem, fugiat quam, obcaecati doloribus
-            voluptatibus commodi suscipit quis fuga provident? Nemo qui modi
-            enim neque asperiores animi dignissimos odit necessitatibus eveniet
-            reiciendis placeat, error, nam explicabo quidem quam aut dolore
-            temporibus natus minima facilis esse. Saepe, voluptatum odio! Eaque
-            asperiores perferendis eligendi consequatur! Dignissimos nostrum
-            nisi sed! Non aliquam eum veniam deserunt nam assumenda praesentium
-            eaque, ipsam possimus. Aliquam ad neque animi, quisquam vitae culpa.
-            Officia suscipit omnis nam reprehenderit, dolorum eum. Ea minus
-            atque praesentium necessitatibus illum. x
+            Amateur radio, or &quot;ham radio,&quot; attracts a diverse community of
+            enthusiasts who are drawn by a shared passion for technology,
+            communication, and service. For many, the appeal lies in the
+            hands-on experience of exploring radio frequencies, building
+            antennas, and experimenting with electronics. It&apos;s a hobby that
+            offers endless opportunities for learning and innovation, making it
+            a perfect fit for those with a curiosity about how things work.
+            <br />
+            <br />
+            Additionally, amateur radio provides a unique platform for global
+            communication, allowing operators to connect with people across the
+            world, often in remote or underserved areas. This global reach
+            fosters a deep sense of connection and community, as operators share
+            knowledge, participate in international events, and engage in
+            friendly competitions.
+            <br />
+            <br />
+            Beyond the technical and social aspects, amateur radio also serves a
+            critical role in public service. Many operators volunteer their
+            skills to support emergency communications during disasters,
+            ensuring that vital information can still flow when other networks
+            are down. This commitment to community safety and preparedness is a
+            significant draw for those who want to contribute positively to
+            society. Whether driven by a love of technology, a desire for global
+            connection, or a commitment to public service, amateur radio offers
+            something for everyone, making it a rewarding and fulfilling hobby.
           </p>
         </div>
       </main>
