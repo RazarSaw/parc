d1b1c44afc9cd330a7954a0ec2064caa7adfd50c
Razar Saw <razar.saw@gmail.com>
2024-08-20 00:42:08 -0600

    Rerouted image source

--- app/ham-radio-resources/ham-radio-info/header.jsx
+++ app/ham-radio-resources/ham-radio-info/header.jsx
@@ -1,7 +1,7 @@
 export default function header() {
   return (
     <header
-      className={`bg-[url('/parc/images/headers/Ham%20Radio%20Info%20Header.png')] bg-no-repeat 2xl:bg-fixed bg-cover 2xl:bg-[100%,90%] min-h-[40vh] grid grid-cols-1 grid-rows-1 justify-center bg-blue-300 bg-blend-multiply`}
+      className={`bg-[url('/parc/images/headers/ham-radio-info.png')] bg-no-repeat 2xl:bg-fixed bg-cover 2xl:bg-[100%,90%] min-h-[40vh] grid grid-cols-1 grid-rows-1 justify-center bg-blue-300 bg-blend-multiply`}
     >
       <div className="backdrop-blur-sm flex flex-col justify-center">
         <div className="container mx-auto p-8">
