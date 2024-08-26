dad2db9a5a3778014d9c4e1578e66439bdb5669f
Razar Saw <razar.saw@gmail.com>
2024-08-26 14:11:38 -0600

    Added banner animation, rerouted URL to membership page, removed 'Interested In Our Network'

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,27 +1,25 @@
 import Link from "next/link";
+import { motion } from "framer-motion";
 
 export default function Header() {
   return (
     <>
       <header className="grid place-items-center gap-8 px-8 py-16 bg-[url('/parc/images/headers/home.png')] bg-cover bg-bottom min-h-[40vh] bg-blend-multiply bg-gray-500 text-center">
         <Link href={"/"} className="hover:scale-[1.05] transition-all">
-          <img src="/parc/images/logos/PARC-banner.png" alt="Parkland Amateur Radio Club banner"/>
+          <motion.img
+            initial={{ x: -9999 }}
+            animate={{ x: 0 }}
+            transition={{ duration: "1.5", }}
+            src="/parc/images/logos/PARC-banner.png"
+            alt="Parkland Amateur Radio Club banner"
+          />
         </Link>
-        <p className="text-3xl text-white font-semibold">
-          Interested In Our Network?
-        </p>
         <div className="flex gap-4 flex-wrap justify-center">
           <a
-            href="https://ve5ufo-radiospace.godaddysites.com/"
+            href="/parc/community-and-support/membership-directors-and-sponsors.html"
             className="bg-blue-400 hover:bg-blue-500 rounded-[8px] p-4 text-white font-semibold hover:scale-[1.05] transition-all"
           >
-            <p>Visit SASKWARN (Wireless Amateur Radio Network)</p>
-          </a>
-          <a
-            href="/parc/community-and-support/membership-directors-and-sponsors"
-            className="bg-blue-400 hover:bg-blue-500 rounded-[8px] p-4 text-white font-semibold hover:scale-[1.05] transition-all"
-          >
-            <p>Membership</p>
+            <p>Join Us & Become A Member!</p>
           </a>
         </div>
       </header>
