d1ea0e246cc981e4246c939cf8ce0c69c682203a
Razar Saw <razar.saw@gmail.com>
2024-08-27 23:45:50 -0600

    Removed obsolete code fixing the centering issue.

--- app/community-and-support/local-info/header.tsx
+++ app/community-and-support/local-info/header.tsx
@@ -4,7 +4,7 @@ export default function Header() {
       className={`bg-[url('/parc/rptlocation1.jpg')] bg-center bg-fixed bg-no-repeat bg-cover min-h-[40vh] grid grid-cols-1 grid-rows-1 justify-center bg-blue-300 bg-blend-multiply`}
     >
       <div className="backdrop-blur-sm flex flex-col justify-center">
-        <div className="container mx-auto px-8 py-16 flex lg:flex-row flex-col flex-wrap items-center gap-8">
+        <div className="container mx-auto px-8 py-16 flex lg:flex-row flex-col gap-8">
           <h1 className="flex-1">Local Info</h1>
         </div>
       </div>
