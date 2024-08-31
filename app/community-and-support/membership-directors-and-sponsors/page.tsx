4207aa1babdd533bb48511072dace22f0d4b0552
Razar Saw <razar.saw@gmail.com>
2024-08-31 15:15:30 -0600

    Reordered sections for members

--- app/community-and-support/membership-directors-and-sponsors/page.tsx
+++ app/community-and-support/membership-directors-and-sponsors/page.tsx
@@ -13,11 +13,10 @@ export default function membershipDirectorsAndSponsors() {
       <Prices />
       <main className="bg-neutral-800">
         <div className="container mx-auto py-16 px-8 flex flex-col gap-16 ">
-          <SilentKeys />
-          <HonoraryMembers />
-          <Members />
           <Directors />
-          <Sponsors />
+          <Members />
+          <HonoraryMembers />
+          <SilentKeys />
         </div>
       </main>
     </>
