7d1829113f6d1ae65348a9e00992e008e2cfc657
Razar Saw <razar.saw@gmail.com>
2024-08-31 21:01:30 -0600

    Changed UI to ensure backdrop blur covered the entire header background

--- app/community-and-support/membership/header.tsx
+++ app/community-and-support/membership/header.tsx
@@ -1,9 +1,9 @@
 export default function Header() {
   return (
     <header
-      className={`bg-[url('/parc/images/headers/ve5pwo-pobetkoff.jpg')] bg-no-repeat bg-fixed bg-cover bg-bottom min-h-[40vh] grid place-items-center bg-black/75 bg-blend-multiply`}
+      className={`bg-[url('/parc/images/headers/ve5pwo-pobetkoff.jpg')] bg-no-repeat bg-fixed bg-cover bg-bottom min-h-[40vh] bg-black/75 bg-blend-multiply grid`}
     >
-      <div className="backdrop-blur-sm flex flex-col justify-center">
+      <div className="backdrop-blur-sm flex flex-col justify-center h-full">
         <div className="container mx-auto px-8 py-16 flex flex-col items-center gap-8">
           <h1 className="flex-1 text-center">Membership</h1>
           <a
